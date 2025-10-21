7b7379cd8b3cffdabbe7a4d97a3c326bef363e06
Dimitry Shanin <dima.shanin@t-a9.com>
2025-10-22 00:53:55 +0300

    add records

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -11,11 +11,19 @@ import {
   UntypedFormGroup,
   Validators
 } from '@angular/forms';
-import {DropDownListModule} from '@syncfusion/ej2-angular-dropdowns';
+import {AutoCompleteModule, DropDownListModule, MultiSelectModule} from '@syncfusion/ej2-angular-dropdowns';
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons'
 import {TextBoxAllModule} from '@syncfusion/ej2-angular-inputs';
 import {PatientStore} from '../shared/store/patient.store';
 import {DialogService} from '../shared/services/dialog.service';
+import {
+  EditService,
+  EditSettingsModel,
+  GridModule,
+  PageService,
+  SortService,
+  ToolbarService
+} from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'app-patient',
@@ -25,7 +33,12 @@ import {DialogService} from '../shared/services/dialog.service';
     NgIf,
     TextBoxAllModule,
     ButtonModule,
+    GridModule,
+    AutoCompleteModule,
+    MultiSelectModule,
+    FormsModule
   ],
+  providers: [EditService, ToolbarService, SortService, PageService],
   standalone: true,
   templateUrl: './patient.component.html',
   styleUrl: './patient.component.scss'
@@ -43,10 +56,25 @@ export class PatientComponent implements OnInit {
   priorityData: string[] = ['Critical', 'Urgent', 'Standard', 'Routine'];
   doctorData: string[] = ['Burger', 'Roi', 'John']; // Example list of doctors
   patientStore = inject(PatientStore);
+
+  recordsData = [];
+  recordsEditSettings:EditSettingsModel = { allowEditing: true, allowAdding: true, allowDeleting: true  };
+  toolbar = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+  orderIDRules = { required: true };
+  customerIDRules = { required: true, minLength: 5 };
+  numericParams = {
+    params: {
+      decimals: 0,
+      format: 'N',
+      showClearButton: true,
+      showSpinButton: false
+    }
+  }
   constructor(private fb: FormBuilder,private dialogService: DialogService) {
     effect(() => {
       const data = this.patientData();
       if(data && this.isPatient(data)){
+        this.recordsData = data.Records;
         this.patientForm = new FormGroup({
           Id: new FormControl<number>({ value: data.Id, disabled: true },[]), // ID is usually read-only
           Name: new FormControl<string>(data.Name, [Validators.required]),
@@ -89,11 +117,30 @@ export class PatientComponent implements OnInit {
     if (this.patientForm.valid) {
       // Get the form value, including the disabled Id
       const patient = { ...this.patientForm.getRawValue() };
-      console.log(patient)
+      patient.Records = this.recordsData;
       this.patientStore.updatePatient(patient);
     } else {
       console.log('error')
     }
     this.dialogService.showDialog(null,false)
+  }
+  beforePaste(event:any){
+    if(event && event.data){
+      event.data.UpdateAt=this.getDate();
+    }
+    console.log(event)
+  }
+  getDate(){
+    const d = new Date();
+    const pad = (n:number) => String(n).padStart(2, '0');
+
+    const month = pad(d.getMonth() + 1);
+    const day = pad(d.getDate());
+    const year = String(d.getFullYear()).slice(-2);
+    const hour = pad(d.getHours());
+    const minute = pad(d.getMinutes());
+    const sec = pad(d.getSeconds());
+
+    return `${month}/${day}/${year} ${hour}:${minute}:${sec}`;
   }
 }
